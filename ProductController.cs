5fd2cf2e4aa4710bae4378e170303929776b44b5
Giang200555 <163760230+Giang200555@users.noreply.github.com>
2025-05-14 12:29:31 +0700

    LAB3

---
+++ ProductController.cs
@@ -0,0 +1,20 @@
+﻿using LAB3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LAB3.Controllers
+{
+    public class ProductController : Controller
+    {
+        private static readonly List<Product> _products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Áo thun đồng phục",  Price = 199000m },
+            new Product { Id = 2, Name = "Quần jeans",         Price = 349000m },
+            new Product { Id = 3, Name = "Giày thể thao",      Price = 499000m },
+        };
+
+        public IActionResult Details()
+        {
+            return View(_products);
+        }
+    }
+}
